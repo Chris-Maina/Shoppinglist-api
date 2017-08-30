c10518f563855a4ab62fb450f7ee292a8d1c135f
Chris-Maina <krismaish34@gmail.com>
2017-08-30 11:25:10 +0300

    [Update]__init__.py to add shoppinglist_edit function

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, jsonify, abort
 
 # local import
 from instance.config import app_config
@@ -10,10 +11,57 @@ from instance.config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
+    """Initialize app"""
+    from app.models import Shoppinglist
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
+    @app.route('/shoppinglists/', methods=['POST', 'GET'])
+    def shoppinglists():
+        """ Handles POST and GET methods"""
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            if name:
+                shoppinglist = Shoppinglist(name=name)
+                shoppinglist.save()
+                response = jsonify({
+                    'id': shoppinglist.id,
+                    'name': shoppinglist.name,
+                    'date_created': shoppinglist.date_created,
+                    'date_modified': shoppinglist.date_modified
+                })
+                response.status_code = 201
+                return response
+        else:
+            # GET
+            shoppinglists = Shoppinglist.get_all()
+            results = []
+            for shoplist in shoppinglists:
+                item = {
+                   'id': shoplist.id,
+                    'name': shoplist.name,
+                    'date_created': shoplist.date_created,
+                    'date_modified': shoplist.date_modified 
+                }
+                results.append(item)
+            
+            response = jsonify(results)
+            response.status_code = 200
+            return response
+    
+    @app.route('/shoppinglists/<int: id>', methods=['PUT', 'GET', 'DELETE'])
+    def shoppinglist_edit(id, **kwargs):
+        # retrieve a shoppinglist by it's ID
+        shoppinglist = Shoppinglist.query.filter_by(id=id).first()
+        if not shoppinglist:
+            # No shopping list ,raise error 404 status code not found
+            abort(404)
+        if request.method == 'DELETE':
+            shoppinglist.delete()
+            return {
+            "message": "Shopping list {} deleted successfully".format(shoppinglist.id)
+        }, 200
     return app
