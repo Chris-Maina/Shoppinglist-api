2b7659409629b08bb5429bad0fcfa6fea2802c7e
Chris-Maina <krismaish34@gmail.com>
2017-08-30 22:26:24 +0300

    [Update]__init__.py to handle user registration

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@
 
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
-from flask import request, jsonify, abort
+from flask import request, jsonify, abort, make_response
+import re
 
 # local import
 from instance.config import app_config
@@ -14,6 +15,7 @@ db = SQLAlchemy()
 def create_app(config_name):
     """Initialize app"""
     from app.models import Shoppinglist
+    from app.models import User
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
@@ -29,9 +31,73 @@ def create_app(config_name):
                                    "Login by sending a POST request to"
                                    " /auth/login/ to get started."})
 
+    @app.route('/auth/register/', methods=['POST', 'GET'])
+    def dummy_register():
+        """Handles registration of users"""
+        if request.method == 'POST':
+            email = str(request.data.get('email'))
+            password = str(request.data.get('password'))
+            if email == "":
+                # check if email is empty, status code bad request 400
+                response = {
+                    'message': 'Please fill email field.'
+                }
+                return make_response(jsonify(response)), 400
+            elif not re.match("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email):
+                # check to see if email meets the above regular expression
+                response = {
+                    'message': 'Please provide a valid email address.'
+                }
+                return make_response(jsonify(response)), 403
+            elif password == "":
+                # check if password is empty, status code bad request 400
+                response = {
+                    'message': 'Please fill password field.'
+                }
+                return make_response(jsonify(response)), 400
+            elif len(password) < 6:
+                response = {
+                    'message': 'Your password should be atleast 6 characters long.'
+                }
+                return make_response(jsonify(response)), 403
+
+            # Query to see if a user already exists
+            user = User.query.filter_by(email=email).first()
+            if not user:
+                # No user, so register
+                try:
+                    # Register user
+                    email = request.data['email']
+                    password = request.data['password']
+                    user = User(email=email, password=password)
+                    user.save()
+                    response = {
+                        'message': 'You have been registered successfully. Please login'
+                    }
+                    # return the response and a status code 201 (created)
+                    return make_response(jsonify(response)), 201
+                except Exception as e:
+                    # when there is an error, return error as message
+                    response = {
+                        'message': str(e)
+                    }
+                    return make_response(jsonify(response)), 401
+            else:
+                # There is a user. Return a message user already exists
+                response = {
+                    'message': 'User already exists. Please login.'
+                }
+                return make_response(jsonify(response)), 202
+        else:
+            # request method GET
+            response = jsonify({"message": "To register,"
+                                           "send a POST request with email and password"
+                                           " to /auth/register/"})
+            return make_response(response), 200
+
 
     @app.route('/shoppinglists/', methods=['POST', 'GET'])
-    def shoppinglists():
+    def dummy_shoppinglists():
         """ Handles POST and GET methods"""
         if request.method == "POST":
             name = str(request.data.get('name', ''))
