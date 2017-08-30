b3c00f513cbe2cd0e4d810f99a492061992a45e9
Chris-Maina <krismaish34@gmail.com>
2017-08-31 00:56:34 +0300

    [Update]test_authentication.py to add login test cases

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -10,7 +10,9 @@ class UserTestCases(unittest.TestCase):
     Test invalid email provided
     Test no email/password provided
     Test password length
-    Test user login
+    Test successful user login
+    Test user login with no email/password
+    Test login GET method
     Test user login with non existent email/password
     """
 
@@ -52,7 +54,7 @@ class UserTestCases(unittest.TestCase):
         self.assertEqual(res.status_code, 400)
         self.assertIn(
             "Please fill email", str(res.data))
-    
+
     def test_no_password_provided(self):
         """Test no password provided"""
         res = self.client().post('/auth/register/',
@@ -69,3 +71,60 @@ class UserTestCases(unittest.TestCase):
         self.assertIn(
             "password should be atleast 6 characters", str(res.data))
 
+    def test_register_get_method(self):
+        """Test Get method in login"""
+        res = self.client().get('/auth/register/')
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("To register", str(res.data))
+
+    def test_user_login(self):
+        """Test user can login after registration"""
+        res = self.client().post('/auth/register/', data=self.user_details)
+        self.assertEqual(res.status_code, 201)
+        login_res = self.client().post('/auth/login/', data=self.user_details)
+        # Test response
+        self.assertIn("You are logged in successfully", login_res.data)
+        self.assertEqual(login_res.status_code, 200)
+        # Get the response in json format
+        result = json.loads(login_res.data.decode())
+        self.assertTrue(result['access_token'])
+
+    def test_login_no_email(self):
+        """Test no email provided when login"""
+        res = self.client().post('/auth/register/', data=self.user_details)
+        self.assertEqual(res.status_code, 201)
+        login_res = self.client().post('/auth/login/', data={
+            'email': '',
+            'password': 'password123'
+        })
+        self.assertIn("Please fill email", str(login_res.data))
+        self.assertEqual(login_res.status_code, 400)
+
+    def test_login_no_password(self):
+        """Test no email provided when login"""
+        res = self.client().post('/auth/register/', data=self.user_details)
+        self.assertEqual(res.status_code, 201)
+        login_res = self.client().post('/auth/login/', data={
+            'email': 'mainachris@gmail.com',
+            'password': ''
+        })
+        self.assertIn("Please fill password", str(login_res.data))
+        self.assertEqual(login_res.status_code, 400)
+
+    def test_login_get_method(self):
+        """Test Get method in login"""
+        login_res = self.client().get('/auth/login/')
+        self.assertEqual(login_res.status_code, 200)
+        self.assertIn("To login", str(login_res.data))
+
+    def test_login_non_existent_user(self):
+        """Tests user login with non existent email&password"""
+        user_details = {
+            'email': "test@gmail.com",
+            'password': "testpassword"
+        }
+        res = self.client().post('/auth/login/', data=user_details)
+        result = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(result['message'],
+                         "Invalid email or password, Please try again")
