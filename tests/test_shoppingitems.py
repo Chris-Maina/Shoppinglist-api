3e67fc5e5569b402a37f599e65ffb7386bfd80f2
Chris-Maina <krismaish34@gmail.com>
2017-08-31 23:39:54 +0300

    [Update]test_shoppingitems.py add test case test_api_edit_no_name function

--- tests/test_shoppingitems.py
+++ tests/test_shoppingitems.py
@@ -122,7 +122,7 @@ class ShoppingItemsTestCases(unittest.TestCase):
                                  data=item)
         self.assertEqual(res2.status_code, 200)
         self.assertIn("sugar", str(res2.data))
-    
+
     def test_api_can_edit_non_existing_item(self):
         """ Test API can edit a non existing item """
         item = {'name': 'sugar'}
@@ -135,6 +135,25 @@ class ShoppingItemsTestCases(unittest.TestCase):
                                  data=item)
         self.assertEqual(res2.status_code, 404)
         self.assertIn("No such item", str(res2.data))
+
+    def test_api_edit_no_name(self):
+        """ Test API cannot edit with no item name provided """
+        item = {'name': ''}
+        res = self.register_login_user_create_shoppinglist()
+        self.assertEqual(res.status_code, 201)
+        # create an item
+        res = self.client().post("/shoppinglists/1/items",
+                                 headers=dict(
+                                     Authorization="Bearer " + self.access_token),
+                                 data=self.shoppingitem)
+        self.assertEqual(res.status_code, 201)
+        # edit item
+        res2 = self.client().put("/shoppinglists/1/items/1",
+                                 headers=dict(
+                                     Authorization="Bearer " + self.access_token),
+                                 data=item)
+        self.assertEqual(res2.status_code, 400)
+        self.assertIn("enter an item name", str(res2.data))
 
     def tearDown(self):
         """teardown all initialized variables."""
