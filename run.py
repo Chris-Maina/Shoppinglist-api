8b441f763ce2e56cb32f191a595568715b20059f
Chris-Maina <krismaish34@gmail.com>
2017-09-20 07:24:02 +0300

    [Chore #[2104056]] Load dotenv from base root

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 """ run.py """
 import os
 from app import create_app
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# load .env in the base root
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 config_name = "development"
 app = create_app(config_name)
