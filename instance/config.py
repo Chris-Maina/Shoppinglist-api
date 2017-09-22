89b7f726ae036643c3156d4e5d543528e78384e0
Chris-Maina <krismaish34@gmail.com>
2017-09-22 10:00:18 +0300

    [Chore #[ 2104056 ] Added environment variables]

--- instance/config.py
+++ instance/config.py
@@ -6,8 +6,8 @@ class Config(object):
     """Parent class configurations"""
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('shop_db')
+    SECRET = "hardworkpaysbychris"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:chris@localhost:5432/shop_api"
 
 
 class DevelopmentConfig(Config):
