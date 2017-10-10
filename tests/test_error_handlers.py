0f11a6331f1ae045e93398b2c6baaf05e2837e06
Chris-Maina <krismaish34@gmail.com>
2017-10-10 17:14:20 +0300

    [Chore #[2104056]] Add test cases for error handlers

---
+++ tests/test_error_handlers.py
@@ -0,0 +1,21 @@
+""" /tests/test_error_handlers.py"""
+from tests.basetest import BaseTest
+
+
+class ErrorHandlersTestCases(BaseTest):
+    """
+    Test 404
+    Test 405
+    Test 500
+    """
+    def test_404_error(self):
+        """Tests 404 error"""
+        res = self.client().get('/auth/register/kbnj')
+        self.assertEqual(res.status_code, 404)
+        self.assertIn("not found", str(res.data))
+
+    def test_405_error(self):
+        """Test 405 error"""
+        res = self.client().put('/auth/register/')
+        self.assertEqual(res.status_code, 405)
+        self.assertIn("Method not allowed ", str(res.data))
