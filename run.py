c64d53747679b60f239c81aa5be55b5d335c31be
Chris-Maina <krismaish34@gmail.com>
2017-10-16 15:46:03 +0300

    [Chore] Implement CORS

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import os
 from app import create_app
 from os.path import join, dirname
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 # load .env in the base root
 dotenv_path = join(dirname(__file__), '.env')
@@ -10,6 +11,7 @@ load_dotenv(dotenv_path)
 
 config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
+CORS(app)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
