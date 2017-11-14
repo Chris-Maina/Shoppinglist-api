92faaeecd9407d57ecc8774182ca9c35669fda38
Chris-Maina <krismaish34@gmail.com>
2017-11-14 16:08:14 +0300

    [Chore] Add config variables for email setup

--- instance/config.py
+++ instance/config.py
@@ -8,6 +8,13 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = os.getenv('MAIL_PORT')
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('DEFAULT_SENDER')
+    APP_URL = os.getenv('APP_URL')
 
 
 class DevelopmentConfig(Config):
